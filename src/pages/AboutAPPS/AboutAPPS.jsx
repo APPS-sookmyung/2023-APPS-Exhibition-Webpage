580c48c8fde53e5501274f8f5505f046d1994239
mxinseo <jade201913@gmail.com>
2023-10-26 18:44:44 +0900

    Del: AboutAPPS, Members 폴더 삭제

--- src/pages/AboutAPPS/AboutAPPS.jsx
+++
@@ -1,7 +0,0 @@
-import * as S from './AboutAPPS.style';
-
-const [AboutAPPS] = ({ children }) => {
-  return <S.StyledAboutAPPS>{children}</S.StyledAboutAPPS>;
-};
-
-export default [AboutAPPS];
--- src/pages/AboutAPPS/AboutAPPS.style.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const [StyledAboutAPPS] = styled.div`
-  font-size: 28px;
-  font-weight: bold;
-`;
