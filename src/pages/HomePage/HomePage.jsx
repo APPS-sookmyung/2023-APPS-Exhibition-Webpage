d9c62d8623a417a5c886065d8da4a7757b5c47bc
mxinseo <jade201913@gmail.com>
2023-10-26 18:55:23 +0900

    Feat: APPS 소개 섹션 개발
    홈 페이지에 APPS 소개 섹션 개발

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -24,6 +24,20 @@ const HomePage = () => {
             <S.SectionDescription>프로젝트는 아래에서 확인할 수 있습니다.</S.SectionDescription>
           </S.MessageSectionInner>
         </S.MessageSection>
+
+        <S.AboutAppsSection>
+          <S.AboutAppsSectionInner>
+            <S.SectionTitle>{'APPS 소개'}</S.SectionTitle>
+            <S.SectionDescription>
+              APPS는 모바일 앱&웹 프로그래밍 동아리로,
+              <br /> 기술과 아이디어를 통해 프로젝트를 진행하며
+              <br />
+              개발 능력을 향상시키고 커뮤니티 내에서 성장합니다.
+              <br />
+              활발한 소통과 피드백을 통해 개인과 공동체 발전을 촉진합니다.
+            </S.SectionDescription>
+          </S.AboutAppsSectionInner>
+        </S.AboutAppsSection>
       </S.Container>
     </PageLayout>
   );
