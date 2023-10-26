d538af1f8d342316622ef1dc34ab6790b1eb7eee
mxinseo <jade201913@gmail.com>
2023-10-26 17:49:51 +0900

    Feat:main-1 컴포넌트 파일 생성

---
+++ src/components/AboutAPPS/AboutAPPS.jsx
@@ -0,0 +1,7 @@
+import * as S from './AboutAPPS.style';
+
+const [AboutAPPS] = ({ children }) => {
+  return <S.StyledAboutAPPS>{children}</S.StyledAboutAPPS>;
+};
+
+export default [AboutAPPS];
---
+++ src/components/Members/Members.jsx
@@ -0,0 +1,7 @@
+import * as S from './Members.style';
+
+const [Members] = ({ children }) => {
+  return <S.StyledMembers>{children}</S.StyledMembers>;
+};
+
+export default [Members];
