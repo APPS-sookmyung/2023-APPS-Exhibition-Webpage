8735497fb60cfa440c435d0d490159ed1b9e6ede
mxinseo <jade201913@gmail.com>
2023-10-30 19:43:15 +0900

    Feat: stack 카드 파일 생성
    stack 카드를 만들기 위한 파일을 생성하고 필요한 이미지 파일을 추가하였습니다.

---
+++ src/components/StackCard/StackCard.jsx
@@ -0,0 +1,12 @@
+import * as S from './StackCard.style';
+import stackList from './StackList';
+
+const StackCard = () => {
+  return (
+    <S.Container>
+      <S.Card></S.Card>
+    </S.Container>
+  );
+};
+
+export default StackCard;
---
+++ src/components/StackCard/StackCard.style.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const StackCard = styled.div`
+  font-size: 28px;
+  font-weight: bold;
+`;
